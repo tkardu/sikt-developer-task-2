d112411631507eabbdc438e9818eb10393108e65
unknown <marina.atkina@gmail.com>
2022-06-11 20:24:45 +0200

    Base func and test

--- src/main/java/no/sikt/calculator/ExpressionEvaluator.java
+++ src/main/java/no/sikt/calculator/ExpressionEvaluator.java
@@ -1,13 +1,74 @@
 package no.sikt.calculator;
 
+import java.util.Stack;
+import java.util.Arrays;
+
 import static java.util.Objects.nonNull;
 
 public class ExpressionEvaluator {
+    Integer result;
     public ExpressionEvaluator(String expression) {
         assert nonNull(expression);
+        try
+        {
+            this.result = processExpression(expression);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+   public Integer processExpression (String expression){
+       Stack<Integer> stackedValues = new Stack<Integer>();
+       String[] tokenized = expression.split(" ");
+       for (String element : tokenized) {
+           assert nonNull(element);
+           if (element.equals("+") || element.equals("-") || element.equals("*") || element.equals("/")) {
+               stackOperation(element, stackedValues);
+
+           } else {
+                   stackedValues.push(Integer.valueOf(element));
+                   //throws an exception
+           }
+       }
+       if (stackedValues.size() == 1){
+           return stackedValues.peek();
+       }
+       else {
+           throw new IndexOutOfBoundsException("Stack size after processing is not 1");
+       }
+   }
+
+
+    public Stack<Integer> stackOperation(String operation, Stack<Integer> stackCalculator) {
+        int value2 = stackCalculator.pop();
+        int value1 = stackCalculator.pop();
+        int operationResult;
+        if (operation.equals("+")) {
+            operationResult = value1 + value2;
+        } else if (operation.equals("-")) {
+            operationResult = value1 - value2;
+        } else if (operation.equals("/")) {
+            if (value2 != 0) {
+                operationResult = value1 / value2;
+            }
+            else{
+                throw new ArithmeticException("The operation value sequence lead to zero division");
+            }
+        } else if (operation.equals("*")) {
+            operationResult = value1 * value2;
+        }
+        else {
+            throw new RuntimeException("An operation was processed wrong");
+        }
+        //TOdo: handle wrong operator
+        stackCalculator.push(operationResult);
+        return stackCalculator;
     }
 
     public Integer getResult() {
-        return 2;
+        assert nonNull(this.result);
+        return this.result;
     }
 }
+
--- src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
+++ src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
@@ -1,5 +1,6 @@
 package no.sikt.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,9 +9,26 @@ public class ExpressionEvaluatorTest {
 
     @Test
     void shouldReturnValueWhenExpressionIsValid()  {
-        var expected = 2;
-        var expression = "1 1 +";
+        var expected = 42;
+        var expression = "6 5 2 + *";
         var actual = new ExpressionEvaluator(expression).getResult();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void zeroDivisionCausedByExpression()  {
+        var expression = "6 5 0 / *";
+        ArithmeticException thrown = Assertions.assertThrows(ArithmeticException.class, () -> {
+            new ExpressionEvaluator(expression).getResult();
+        });
+        Assertions.assertEquals("The operation value sequence lead to zero division", thrown.getMessage());
+    }
+
+    @Test
+    void emptyExpression()  {
+        var expression = "";
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            new ExpressionEvaluator(expression).getResult();
+        });
     }
 }
